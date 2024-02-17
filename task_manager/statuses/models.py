da018d6fd3a36cdb8feb173b3384bbc00f5b1ed4
prStudentka <bk_nvk@mail.ru>
2024-02-17 19:12:20 +0000

    add func __str__ for task.form

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -5,7 +5,11 @@ from django.utils.translation import gettext_lazy as _
 class Status(models.Model):
     name = models.CharField(max_length=255, unique=True, blank=False, verbose_name=_('Name'))
     created_at = models.DateTimeField(auto_now_add=True)
-	
+
+
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = _('Status')
         verbose_name_plural = _('Statuses')
