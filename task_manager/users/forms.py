e815f147375aff19975e9d197273b8b193834f2b
prStudentka <bk_nvk@mail.ru>
2024-02-23 07:45:42 +0000

    fixed required for field

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -4,7 +4,13 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class RegistrationForm(UserCreationForm):
-	
+
+
     class Meta(UserCreationForm.Meta):
         model = CustomUser
+
         fields = ('first_name', 'last_name', 'username','password1', 'password2')
+        widgets = {
+                'first_name': forms.TextInput(attrs={'required': True}),
+				'last_name': forms.TextInput(attrs={'required': True}),
+        }
