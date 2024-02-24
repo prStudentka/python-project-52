0ce00138eedb616fec1c1828f3102ee26412d0e2
prStudentka <bk_nvk@mail.ru>
2024-02-24 18:41:20 +0000

    fixed test labels

--- task_manager/labels/tests.py
+++ task_manager/labels/tests.py
@@ -16,9 +16,9 @@ class LabelCrudTest(TestCase):
         data = {
             'name': 'worked',
         }
-        url = reverse_lazy('create label')
+        url = reverse_lazy('marked:create label')
         response = self.client.post(url, data, format='json')
-        self.assertRedirects(response, reverse_lazy('label_index'), status_code=302)
+        self.assertRedirects(response, reverse_lazy('marked:labels_index'), status_code=302)
         label = Label.objects.get(name=data['name'])
         self.assertEqual(label.name, data['name'])
 
@@ -26,24 +26,24 @@ class LabelCrudTest(TestCase):
         update_data = {
             'name': 'fix it'
         }
-        url = reverse_lazy('update label', args=[self.label_test.pk])
+        url = reverse_lazy('marked:update label', args=[self.label_test.pk])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         response = self.client.post(url, data=update_data)
         self.label_test.refresh_from_db()
         self.assertEqual(self.label_test.name, update_data['name'])
-        url_back = reverse_lazy('label_index')
+        url_back = reverse_lazy('marked:labels_index')
         self.assertRedirects(response, url_back, status_code=302)
 
     def test_delete_label(self):
         before_count_status = len(Label.objects.all())
         self.assertEqual(before_count_status, 1)
         key = self.label_test.pk
-        url = reverse_lazy('delete label', kwargs={'pk': key})
+        url = reverse_lazy('marked:delete label', kwargs={'pk': key})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         response = self.client.delete(url)
         with self.assertRaises(ObjectDoesNotExist):
             Label.objects.get(pk=key)
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse_lazy('label_index'))
+        self.assertRedirects(response, reverse_lazy('marked:labels_index'))
