4d80ba16c4923e961fd17dfb91c8788f891e6dd2
prStudentka <bk_nvk@mail.ru>
2024-02-24 20:30:42 +0000

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -93,16 +93,18 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600),
-   # 'default_': {
-      # "ENGINE": "django.db.backends.sqlite3",
-      # "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    # }
+    'default': {
+       "ENGINE": 'django.db.backends.postgresql_psycopg2',
+     },
+    'default_': {
+       "ENGINE": "django.db.backends.sqlite3",
+       "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+     }
 }
 
-
+DATABASES['default'] = dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600)
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
